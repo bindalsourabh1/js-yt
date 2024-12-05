217624e9b0d3ede083f416e34324803190676bc6
Sourabh Bindal <95605311+bindalsourabh1@users.noreply.github.com>
2024-12-05 11:56:44 +0000

    dataTypes

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,10 @@
+let score = null
+console.log(typeof score)
+console.log(typeof(score))
+
+let converted = Number(score);
+console.log(typeof(converted))
+
+let num = 0;
+let c2 = Boolean(num)
+console.log(c2)
