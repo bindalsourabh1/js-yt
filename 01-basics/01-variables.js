3fe7ce3df90720447a80cf8b2c7741548032abda
Sourabh Bindal <95605311+bindalsourabh1@users.noreply.github.com>
2024-12-05 11:03:45 +0000

    datatype

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountID = 123456;
+let accountEmail = "sourabh@gmail.com";
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// console.log(accountEmail)
+console.table([accountEmail, accountID, accountPassword])
+
+// null standalone value hai
+// undefined
+//symbol => unique
+console.log(typeof null)
+console.log(typeof undefined);
